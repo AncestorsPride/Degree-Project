d93027733b6a71513bc88ae29a0af849a652a954
AncestorsPride <53060818+AncestorsPride@users.noreply.github.com>
2023-01-11 22:48:57 -0500

    PreFinalMasterCode

--- RaspberryPiCore/TestMasterProgram.py
+++ RaspberryPiCore/TestMasterProgram.py
@@ -3,7 +3,9 @@ import sys
 import os
 import math
 import datetime
-import IMU
+import serial
+import RPi.GPIO as GPIO
+#import IMU
 import board
 import digitalio
 from adafruit_motorkit import MotorKit
@@ -11,10 +13,22 @@ from adafruit_rgb_display.rgb import color565
 from adafruit_rgb_display import st7789
 from gpiozero import CPUTemperature
 
-RAD_TO_DEG = 57.29578
-M_PI = 3.14159265358979323846
-G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-AA =  0.40      # Complementary filter constant
+
+#################################################################################################################################################
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23, GPIO.OUT)
+ 
+ser = serial.Serial(
+     port='/dev/ttyUSB0',
+     baudrate=9600,
+     parity=serial.PARITY_NONE,
+     stopbits=serial.STOPBITS_ONE,
+     bytesize=serial.EIGHTBITS,
+     timeout=1
+     )
+counter=0
+
 
 #___________________________________________________________________________________________________________________________________________________________________
 
@@ -45,49 +59,13 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
-kit1=MotorKit(address=0x61)#chassis
-kit2=MotorKit(address=0x62)#lifting mechanism
+kit1=MotorKit(address=0x61)#address=0x61)#chassis
+# kit2=MotorKit(address=0x62)#lifting mechanism
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-magXmin =  0
-magYmin =  0
-magZmin =  0
-magXmax =  0
-magYmax =  0
-magZmax =  0
 
 
-
-'''
-Here is an example:
-magXmin =  -1748
-magYmin =  -1025
-magZmin =  -1876
-magXmax =  959
-magYmax =  1651
-magZmax =  708
-Dont use the above values, these are just an example.
-'''
-############### END Calibration offsets #################
-
-
-
-gyroXangle = 0.0
-gyroYangle = 0.0
-gyroZangle = 0.0
-CFangleX = 0.0
-CFangleY = 0.0
-
-
-IMU.detectIMU()     #Detect if BerryIMU is connected.
-if(IMU.BerryIMUversion == 99):
-    print(" No BerryIMU found... exiting ")
-    sys.exit()
-IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
-
-
-a = datetime.datetime.now()
 
 
 #kit1.motor1.throttle=0.1
@@ -95,75 +73,12 @@ a = datetime.datetime.now()
 #kit1.motor3.throttle=-0.1
 #kit1.motor4.throttle=-0.1
 
-#Read the accelerometer,gyroscope and magnetometer values
-ACCx = IMU.readACCx()
-ACCy = IMU.readACCy()
-ACCz = IMU.readACCz()
-GYRx = IMU.readGYRx()
-GYRy = IMU.readGYRy()
-GYRz = IMU.readGYRz()
-MAGx = IMU.readMAGx()
-MAGy = IMU.readMAGy()
-MAGz = IMU.readMAGz()
-    
-##Calculate loop Period(LP). How long between Gyro Reads
-b = datetime.datetime.now() - a
-a = datetime.datetime.now()
-LP = b.microseconds/(1000000*1.0)
-outputString = "Loop Time %5.2f " % ( LP )
 
-
-#Convert Gyro raw to degrees per second
-rate_gyr_x =  GYRx * G_GAIN
-rate_gyr_y =  GYRy * G_GAIN
-rate_gyr_z =  GYRz * G_GAIN
-
-
-#Calculate the angles from the gyro.
-gyroXangle+=rate_gyr_x*LP
-gyroYangle+=rate_gyr_y*LP
-gyroZangle+=rate_gyr_z*LP
-
-
-#Convert Accelerometer values to degrees
-AccXangle =  (math.atan2(ACCy,ACCz)*RAD_TO_DEG)
-AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
-    
-    
-#Complementary filter used to combine the accelerometer and gyro values.
-CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
-CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
-
-       
-#convert the values to -180 and +180
-if AccYangle > 90:
-    AccYangle -= 270.0
-elif AccYangle < 90:
-    AccYangle += 90.0
-
-#Calculate heading
-heading = 180 * math.atan2(MAGy,MAGx)/M_PI
-
-#Only have our heading between 0 and 360
-if heading < 0:
-    heading += 360
-
-####################################################################
-###################Tilt compensated heading#########################
-####################################################################
-#Normalize accelerometer raw values.
-accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-
-
-#Calculate pitch and roll
-pitch = math.asin(accXnorm)
-roll = -math.asin(accYnorm/math.cos(pitch))
  
 def forward(speed, t):
     """_summary_
     Args:
-        speed (double):
+        speed (double/float):
         ranges from 0 to +1.0
         indicating the speed of the motors
         t is the time until which motors are ON
@@ -179,8 +94,19 @@ def forward(speed, t):
         kit1.motor2.throttle=0
         kit1.motor3.throttle=0
         kit1.motor4.throttle=0
-
-
+        
+    elif speed < 0:
+        print('Going forward-')
+        kit1.motor1.throttle=speed
+        kit1.motor2.throttle=speed
+        kit1.motor3.throttle=-abs(speed)
+        kit1.motor4.throttle=-abs(speed)
+        time.sleep(t)
+        kit1.motor1.throttle=0
+        kit1.motor2.throttle=0
+        kit1.motor3.throttle=0
+        kit1.motor4.throttle=0
+        
     else:
         return "Forward can only be a positive double from 0 to +1.0! Time has to be greater than 0!"
 
@@ -267,27 +193,28 @@ def retract(lift_speed):
     
 #def turn_on_place(left,right):
     
-def turn_right(current_angle, angle, phase_speed):
+def turn_right(r_speed,l_speed,t):#current_angle, angle, phase_speed):
     """_summary_
     Args:
         current_angle (dobule): datum angle = 0 deg 
         angle (int): 4 inputs are accepted (90, 180, 270, 360)
         phase_speed (str): can be slow, medium or fast
     """
-    datum_angle = gyroZangle
-    speed = 0.20
+    #atum_angle = gyroZangle
     
-    while datum_angle != (gyroZangle + 90.0):
+    
+   # while datum_angle != (gyroZangle + 90.0):
         
-        kit1.motor1.throttle=-0.20
-        kit1.motor2.throttle=-0.20
-        kit1.motor3.throttle=0.20
-        kit1.motor4.throttle=0.20
-        
-    kit1.motor1.throttle=0
-    kit1.motor2.throttle=0
-    kit1.motor3.throttle=0
-    kit1.motor4.throttle=0
+    kit1.motor1.throttle=r_speed
+    kit1.motor2.throttle=r_speed
+    kit1.motor3.throttle=-l_speed
+    kit1.motor4.throttle=-l_speed
+    time.sleep(t)
+    kit1.motor1.throttle=0.0
+    kit1.motor2.throttle=0.0
+    kit1.motor3.throttle=0.0
+    kit1.motor4.throttle=0.0
+    
         
     
 
@@ -296,135 +223,85 @@ def turn_right(current_angle, angle, phase_speed):
 
 while True:
     
-    #Read the accelerometer,gyroscope and magnetometer values
-    ACCx = IMU.readACCx()
-    ACCy = IMU.readACCy()
-    ACCz = IMU.readACCz()
-    GYRx = IMU.readGYRx()
-    GYRy = IMU.readGYRy()
-    GYRz = IMU.readGYRz()
-    MAGx = IMU.readMAGx()
-    MAGy = IMU.readMAGy()
-    MAGz = IMU.readMAGz()
     
-      ##Calculate loop Period(LP). How long between Gyro Reads
-    b = datetime.datetime.now() - a
-    a = datetime.datetime.now()
-    LP = b.microseconds/(1000000*1.0)
-    outputString = "Loop Time %5.2f " % ( LP )
+    x=ser.readline()# .strip() .readline() 
 
-
-    #Convert Gyro raw to degrees per second
-    rate_gyr_x =  GYRx * G_GAIN
-    rate_gyr_y =  GYRy * G_GAIN
-    rate_gyr_z =  GYRz * G_GAIN
-
-
-    #Calculate the angles from the gyro.
-    gyroXangle+=rate_gyr_x*LP
-    gyroYangle+=rate_gyr_y*LP
-    gyroZangle+=rate_gyr_z*LP
-
-
-    #Convert Accelerometer values to degrees
-    AccXangle =  (math.atan2(ACCy,ACCz)*RAD_TO_DEG)
-    AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
     
-    
-    #Complementary filter used to combine the accelerometer and gyro values.
-    CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
-    CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
-
-       
-    #convert the values to -180 and +180
-    if AccYangle > 90:
-        AccYangle -= 270.0
-    elif AccYangle < 90:
-        AccYangle += 90.0
-
-    #Calculate heading
-    heading = 180 * math.atan2(MAGy,MAGx)/M_PI
-
-    #Only have our heading between 0 and 360
-    if heading < 0:
-        heading += 360
-
-    ####################################################################
-    ###################Tilt compensated heading#########################
-    ####################################################################
-    #Normalize accelerometer raw values.
-    accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-    accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-
-
-    #Calculate pitch and roll
-    pitch = math.asin(accXnorm)
-    roll = -math.asin(accYnorm/math.cos(pitch))
-
-
-    #Calculate the new tilt compensated values
-    #The compass and accelerometer are orientated differently on the the BerryIMUv1, v2 and v3.
-    #This needs to be taken into consideration when performing the calculations
-
-    #X compensation
-    if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
-        magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
-    else:                                                                #LSM9DS1
-        magXcomp = MAGx*math.cos(pitch)-MAGz*math.sin(pitch)
-
-    #Y compensation
-    if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
-        magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)
-    else:                                                                #LSM9DS1
-        magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)+MAGz*math.sin(roll)*math.cos(pitch)
-
-
-
-
-    #Calculate tilt compensated heading
-    tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
-
-    if tiltCompensatedHeading < 0:
-        tiltCompensatedHeading += 360
-
-
-    ##################### END Tilt Compensation ########################
-    
-
     #------------------------------------------------------------
  
     
     if buttonA.value and buttonB.value:
+        
         backlight.value = False  # turn off backlight
     else:
         backlight.value = True  # turn on backlight
-    if buttonB.value and not buttonA.value:  # just button A pressed
-        display.fill(color565(255, 0, 0))  # red
-        forward(0.2, 3.0)
-        print(cpu.temperature) #Anything under 50C is acceptable
-        print(gyroZangle)
         
-    elif buttonA.value and not buttonB.value:  # just button B pressed
-        display.fill(color565(0, 0, 255))  # blue
-        while gyroZangle != (gyroZangle - 90.0):
-             kit1.motor1.throttle=0.25
-             kit1.motor2.throttle=0.25
-             kit1.motor3.throttle=-0.25/2
-             kit1.motor4.throttle=-0.25/2
-             print(gyroZangle)
-        else:
-             kit1.motor1.throttle=0
-             kit1.motor2.throttle=0
-             kit1.motor3.throttle=0
-             kit1.motor4.throttle=0
-       # turn_right(0,0,0)
-       # backward(0.2, 3.0)
-       # print(cpu.temperature) #Anything under 50C is acceptable
-       # print(gyroZangle)
         
-    if not buttonA.value and not buttonB.value:  # none pressed
-        display.fill(color565(0, 255, 0))  # green
+    if x.decode('utf-8') == 'f':
+        #x[:2]+x[-1]
+        #GPIO.output(23, GPIO.HIGH)
+        #time.sleep(3)
+        send = ('END03 Going Forward')
+        r=send.encode()
+        #r=bytes(send) 
+        ser.write(r)
+        #time.sleep(3)
+        forward(0.4, 3.0)
+        print('Moved the bot forward')
+       
+    elif x.decode('utf-8')=='b':
+        send = ('END03 Going Backward')
+        r=send.encode()
+        #r=bytes(send) 
+        ser.write(r)
+        #time.sleep(3)
+        backward(0.4, 3.0)
+        print('Moved the bot backward')
         
+    elif x.decode('utf-8')=='r':
+        send = ('END03 Turning Right')
+        r=send.encode()
+        #r=bytes(send) 
+        ser.write(r)
+        #time.sleep(3)
+        turn_right(0.95,0.15,2.8 )
+        print('Turning Right')
+        
+    elif x.decode('utf-8')=='rb':
+        send = ('END03 Turning Right-Back')
+        r=send.encode()
+        #r=bytes(send) 
+        ser.write(r)
+        #time.sleep(3)
+        turn_right(-0.95,-0.15,2.8 )
+        print('Turning Right-Back')
+        
+    else:
+        #GPIO.output(23, GPIO.LOW)
+        #print(x)
+        #print(ser.name) Printing /dev/ttyUSB0
+        #time.sleep(0.125)
+        print('I love you!')
+        print('No change has been detected!!!')
+        print(x,len(x),type(x))
+        print(type(x.decode('utf-8')))
+#         
+#     if buttonB.value and not buttonA.value:  # just button A pressed
+#         display.fill(color565(255, 0, 0))  # red
+#         forward(0.4, 3.0)
+#         print(cpu.temperature) #Anything under 50C is acceptable
+# #         
+#     elif buttonA.value and not buttonB.value:  # just button B pressed
+#         display.fill(color565(0, 0, 255))  # blue
+#         turn_right(0.95,0.15,2.8  )
+# #        # turn_right(0,0,0)
+# #        # backward(0.2, 3.0)
+#         print('Hello, World!') #Anything under 50C is acceptable
+# #        # print(gyroZangle)
+# #         
+# #     if not buttonA.value and not buttonB.value:  # none pressed
+# #         display.fill(color565(0, 255, 0))  # green
+# #         
 
 #if buttonB.value and not buttonA.value:  # just button A pressed
    # display.fill(color565(255, 0, 0))  # red
